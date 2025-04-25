5f04a90804e7a0981a281a1db264f8cd623bfa32
jbob9 <berlenskyoliver99@gmail.com>
2025-04-25 14:25:06 -0400

    add sanity integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: 'avd06zyn',
+      dataset: 'production',
+      useCdn: false,
+      apiVersion: "2025-04-20",
+      studioBasePath: '/studio' // If you want to access the Studio on a route
+    }),
+    react()
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
